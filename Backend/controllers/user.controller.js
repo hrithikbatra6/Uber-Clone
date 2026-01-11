a7cea617bee5e499934121a590a8f190bb23bb79
Hritik Batra <hritikbatra@trentu.ca>
2026-01-11 05:41:59 +0530

    Auth Middleware working

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -50,6 +50,17 @@ module.exports.loginUser = async (req, res, next) => {
     const token = user.generateAuthToken();
 
     res.cookie('token', token);
+    // res.cookie('token', token, {
+    //     httpOnly: true,
+    //     secure: process.env.NODE_ENV === 'production',
+    //     maxAge: 3600000
+    // });
 
     res.status(200).json({ token, user });
+}
+
+module.exports.getUserProfile = async (req, res, next) => {
+
+    res.status(200).json(req.user);
+
 }
