836ea2a0fa06b1ce80a21ae866018d480ee2d0fb
Hritik Batra <hritikbatra@trentu.ca>
2026-01-12 00:39:49 +0530

    Captain routes ready

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -1,4 +1,4 @@
-const blacklistTokenModel = require('../models/blacklistToken.model');
+const blacklistTokenModel = require('../models/blackListToken.model');
 const userModel = require('../models/user.model');
 const userService = require('../services/user.service');
 const { validationResult } = require('express-validator');
