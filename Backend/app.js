bf033017f4ef713adc7484800be4566854076252
Hritik Batra <hritikbatra@trentu.ca>
2026-01-11 23:47:11 +0530

    captain register

--- Backend/app.js
+++ Backend/app.js
@@ -6,6 +6,7 @@ const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const connectToDb = require('./db/db');
 const userRoutes = require('./routes/user.routes');
+const captainRoutes = require('./routes/captain.routes');
 
 connectToDb();
 
@@ -25,6 +26,7 @@ app.get('/', (req, res) => {
 
 app.use('/users', userRoutes);
 
+app.use('/captains', captainRoutes);
 
 module.exports = app
 
